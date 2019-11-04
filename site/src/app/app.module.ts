aac9415631490f887b039dad79bfe21d34559ea5
Maxime <maxou3101@gmail.com>
2019-11-04 17:28:31 +0100

    header & footer end

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccueilComponent
+    AccueilComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
